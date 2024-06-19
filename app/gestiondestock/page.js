e4c684008a70ab27e46d40d3296ea28a15471d3b
abraham241 <huguesmboundou@gmail.com>
2024-06-19 16:46:19 +0100

    initalisation des routes

---
+++ app/gestiondestock/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>Bienvenue sur la page de gestion de stock</div>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Sidebar from "@/components/Sidebar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
       <Sidebar />
       <h1>Hello world</h1>
     </main>
