c21461a50c9d823eb562aefe0a61e1a2bf751539
abraham241 <huguesmboundou@gmail.com>
2024-06-19 16:32:12 +0100

    initialisation

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Sidebar from "@/components/Sidebar";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Sidebar />
+      <h1>Hello world</h1>
+    </main>
+  );
+}
---
+++ components/Sidebar.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { LuLayoutDashboard } from "react-icons/lu";
+import { FaCartArrowDown } from "react-icons/fa6";
+import { FaFileInvoiceDollar } from "react-icons/fa";
+import { IoSettingsOutline } from "react-icons/io5";
+import { IoIosLogOut } from "react-icons/io";
+import Link from 'next/link';
+
+export default function Sidebar() {
+  return (
+    <>
+        <div className='bg-yellow-300 h-screen w-52'>
+            <div>
+                <img src="../public/images/logo.png" alt="logo" width={150}   height={150} />
+            </div>
+            <div className='flex flex-col gap-2 align-items-right pt-44'>
+                <div className='flex justify-center items-center gap-2'>
+                    <LuLayoutDashboard />
+                    <Link href="/">Dashboard</Link>
+                </div>
+                <div className='flex justify-center items-center gap-2'>
+                    <FaCartArrowDown />
+                    <Link href="/dashboard">Gestion de stock</Link>
+                </div>
+                <div className='flex justify-center items-center gap-2'>
+                    <FaFileInvoiceDollar />
+                    <Link href="/">Transactions</Link>
+                </div>
+                <div className='flex justify-center items-center gap-2'>
+                    <IoSettingsOutline />
+                    <Link href="/">Paramètres</Link>
+                </div>
+                <div className='flex justify-center items-center gap-2'>
+                    <IoIosLogOut />
+                    <Link href="/">Se déconnecter</Link>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
