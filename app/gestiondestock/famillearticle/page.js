10e3cf7ace8f1c0f2d9a2026b4e5bc8eae4f8e1f
abraham241 <huguesmboundou@gmail.com>
2024-06-20 16:19:53 +0100

    finition de la page famille d'article & de le recherchebar

--- app/gestiondestock/famillearticle/page.js
+++ app/gestiondestock/famillearticle/page.js
@@ -1,24 +1,58 @@
-import React from 'react'
-import Sidebar from '@/components/Sidebar'
-import Searchbar from '@/components/Searchbar'
+import React from "react";
+import Sidebar from "@/components/Sidebar";
+import Searchbar from "@/components/Searchbar";
+import Recherchebar from "@/components/Recherchebar";
+import { MdDelete } from "react-icons/md";
+import { IoIosAddCircleOutline } from "react-icons/io";
+import Link from "next/link";
 
-export default function  famillearticle() {
+export default function famillearticle() {
   return (
     <>
-      <div className=' flex bg-gray-100'>
-        <div className=''>
+      <div className=" flex bg-gray-100">
+        {/* appel de la sidebar */}
+        <div className="">
           <Sidebar />
         </div>
-        <div className='w-full'>
-          <div className=' h-24 '>
+
+        <div className="w-full">
+          {/* appel de la barre de recherche */}
+          <div className=" h-24 ">
             <Searchbar />
           </div>
-          <div className='px-10 pt-5'>
-            <h1 className='text-3xl font-bold'>Famille Articles</h1>
+          <div className="px-10 pt-5">
+            <h1 className="text-3xl font-bold">Famille Articles</h1>
+          </div>
+
+          <div className="flex justify-end px-10 py-7">
+            <div className=" pt-5 flex gap-4">
+              <button
+                type="button"
+                class="text-white bg-orange-500 flex gap-2 items-center   font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2  "
+              >
+                <div className="text-xl">
+                  <MdDelete />
+                </div>
+                Suprimer
+              </button>
+              <button
+                type="button"
+                class="text-white bg-yellow-300 flex gap-2 items-center   font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2  "
+              >
+                <div className="text-xl">
+                  <IoIosAddCircleOutline />
+                </div>
+                <Link href="/">Ajouter une famille</Link>
+              </button>
+            </div>
+          </div>
+
+          {/* appel de la barre de recherche des familles article */}
+          <div className="px-10 pt-5">
+            <Recherchebar />
           </div>
         </div>
       </div>
-      
     </>
-  )
+  );
 }
