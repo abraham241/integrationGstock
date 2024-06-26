f4053c75821e1a33754ea16c9449af8ed91deb5e
ZINGAClenn01 <105050872+ZINGAClenn01@users.noreply.github.com>
2024-06-26 16:03:22 +0100

    reglage de l'erreur

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import React, { useState } from 'react';
 import { LuLayoutDashboard } from "react-icons/lu";
 import { FaCartArrowDown } from "react-icons/fa";
@@ -7,9 +7,6 @@ import { IoSettingsOutline } from "react-icons/io5";
 import { IoIosLogOut } from "react-icons/io";
 import Link from 'next/link';
 import Image from 'next/image';
-// import logo from '/images/logo.png';
-
-
 
 export default function Sidebar() {
     const [isOpen, setIsOpen] = useState(false);
@@ -23,7 +20,7 @@ export default function Sidebar() {
     };
 
     return (
-        <div className='bg-yellow-300 h-[670px] w-72 flex  flex-col items-start p-10'>
+        <div className='bg-yellow-300 h-[670px] w-72 flex flex-col items-start p-10'>
             <div className='mb-[40%] '>
                 <Image src="/logo.png" alt="logo" width={150} height={150} />
             </div>
@@ -32,27 +29,26 @@ export default function Sidebar() {
                     <LuLayoutDashboard />
                     <Link href="/">Dashboard</Link>
                 </div>
-                <div className='flex justify-start items-center gap-1 w-56  hover:text-white pl-2'>
-                    
+                <div className='flex justify-start items-center gap-1 w-56 hover:text-white pl-2'>
                     <div className="relative inline-block">
                         <button
                             type="button"
-                            className=" py-2 text-black gap-2 p-1 text-sm inline-flex items-center h-12  hover:text-white hover:bg-slate-800"
+                            className="py-2 text-black gap-2 p-1 text-sm inline-flex items-center h-12 hover:text-white hover:bg-slate-800"
                             onClick={toggleDropdown}
-                        ><FaCartArrowDown />
+                        >
+                            <FaCartArrowDown />
                             Gestion de stocks
                             <svg className={`w-4 h-4 ml-2 transition-transform ${isOpen ? 'xl:-rotate-360' : 'xl:-rotate-90'}`} viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path fillRule="evenodd" clipRule="evenodd" d="M14.95 9.95a1 1 0 011.414 1.414l-4.95 4.95a1 1 0 01-1.414 0l-4.95-4.95a1 1 0 111.414-1.414L10 14.086l4.536-4.536a1 1 0 011.414 0z" />
                             </svg>
                         </button>
-
                         {isOpen && (
-                            <div className="origin-top-right  right-0 mt-2 w-44 ">
+                            <div className="origin-top-right right-0 mt-2 w-44">
                                 <ul role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                                     <li>
                                         <Link
                                             href="/gestiondestock/famillearticle"
-                                            className="block px-4 py-2 text-sm text-black "
+                                            className="block px-4 py-2 text-sm text-black"
                                             onClick={() => setIsOpen(false)}
                                         >
                                             Familles d'articles
@@ -61,7 +57,7 @@ export default function Sidebar() {
                                     <li>
                                         <Link
                                             href="/gestiondestock/lapagearticle"
-                                            className="block px-4 py-2 text-sm text-black "
+                                            className="block px-4 py-2 text-sm text-black"
                                             onClick={() => setIsOpen(false)}
                                         >
                                             Articles
@@ -81,22 +77,21 @@ export default function Sidebar() {
                         )}
                     </div>
                 </div>
-                <div className='flex justify-start items-center gap-1   hover:text-white p-2 w-56'>
-                    
+                <div className='flex justify-start items-center gap-1 hover:text-white p-2 w-56'>
                     <div className="relative inline-block">
                         <button
                             type="button"
-                            className="p-1  py-2 text-black gap-2 text-sm inline-flex items-center  hover:text-white hover:bg-slate-800"
+                            className="p-1 py-2 text-black gap-2 text-sm inline-flex items-center hover:text-white hover:bg-slate-800"
                             onClick={toggleDropdownTransx}
-                        ><FaFileInvoiceDollar /> 
+                        >
+                            <FaFileInvoiceDollar />
                             Transactions
                             <svg className={`w-4 h-4 ml-2 transition-transform ${isOpenTransax ? 'xl:-rotate-360' : 'xl:-rotate-90'}`} viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path fillRule="evenodd" clipRule="evenodd" d="M14.95 9.95a1 1 0 011.414 1.414l-4.95 4.95a1 1 0 01-1.414 0l-4.95-4.95a1 1 0 111.414-1.414L10 14.086l4.536-4.536a1 1 0 011.414 0z" />
                             </svg>
                         </button>
-
                         {isOpenTransax && (
-                            <div className="origin-top-right  right-0 mt-2 w-44 ">
+                            <div className="origin-top-right right-0 mt-2 w-44">
                                 <ul role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                                     <li>
                                         <Link
@@ -113,10 +108,9 @@ export default function Sidebar() {
                                             className="block px-4 py-2 text-sm text-black"
                                             onClick={() => setIsOpenTransax(false)}
                                         >
-                                        reclamations
+                                            Réclamations
                                         </Link>
                                     </li>
-                                    
                                 </ul>
                             </div>
                         )}
@@ -134,4 +128,3 @@ export default function Sidebar() {
         </div>
     );
 }
-
